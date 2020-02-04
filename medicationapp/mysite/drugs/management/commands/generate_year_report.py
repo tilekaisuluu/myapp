8ba3072ab4cc6ce89b05a7a676214f85f79e3ed0
Tilek Aisuluu <aisuluuisabekova@gmail.com>
2020-02-04 12:05:18 +0000

    edit import side effect command and create year report

--- medicationapp/mysite/drugs/management/commands/generate_year_report.py
+++ medicationapp/mysite/drugs/management/commands/generate_year_report.py
@@ -7,22 +7,26 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument(
             '--year',
-            type=int,
+            type=str,
             help='Year for which to generate the report'
         )
 
     def handle(self, *args, **options):
 
         # find all medication with date_on_market year same as year specified in CLI (look at https://docs.djangoproject.com/en/3.0/intro/tutorial02/)
+        year = options['year']
+        get_medication = Medication.objects.filter(date_on_market__year=year).values_list('medication_name', 'manufacturer', 'date_on_market')
 
         # print them all with columns: (google search: "python string interpolation tutorial")
             # name :: manufacturer :: date_on_market
             # xyz :: Another Drug Company :: 2000-02-01
+        print('%s' % get_medication)
 
         # find all sideeffects with date_published year same as year specified in CLI
+        get_side_effect = SideEffect.objects.filter(date_published__year=year).values_list('side_effect')
 
         # print them all with columns:
             # side_effect -> drug_name
             # headache -> aspirin
-
+        print('%s' % get_side_effect)
         pass
--- medicationapp/mysite/drugs/management/commands/import_side_effects_data.py
+++ medicationapp/mysite/drugs/management/commands/import_side_effects_data.py
@@ -12,30 +12,41 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
+        # get the name to import from arguments
         side_effect_data_filename = options['side_effects_data_file']
 
+        # check if file is not empty
         if side_effect_data_filename:
             content_lines = []
-
+            # open file for reading
             with open(side_effect_data_filename, 'r') as f:
+                # skip first line
                 next(f)
+                # reading all lines
                 content_lines = f.readlines()
 
 
-
+            # loop through all rows
             for row in content_lines:
+                # split the rows
                 row = row.split(',')
-
+                # get the medication name
                 medicationName = row[0].strip()
-
+                # filter
                 med = Medication.objects.filter(medication_name=medicationName).first()
-
+                # if medication is not exist in database print error, if exist continue
                 if not med:
                     print('error: can not find medication %s associated with side effect' % medicationName)
                     continue
-
+                # create SideEffect object
                 sideEffect = SideEffect()
+                # add all fields
                 sideEffect.medication = med
+                sideEffect.side_effect = row[1].strip()
+                sideEffect.date_published = row[2].strip()
+                # save sideEffect to database
+                sideEffect.save()
+                print(sideEffect.date_published)
 
         else:
             print('Error: specify file')
