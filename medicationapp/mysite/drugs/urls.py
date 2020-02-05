9ea64b60c0e0351d1e4cc2237f447983c327b591
Dauren Tatubaev <dauren_tatubaev@cable.comcast.com>
2020-02-05 07:50:17 -0800

    adding fixes for template

--- medicationapp/mysite/drugs/urls.py
+++ medicationapp/mysite/drugs/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.MedicationListView.as_view(), name='list' ),
-    path('<int:pk>/detail/', views.MedicationDetailView.as_view(), name='detail'),
+    path('list/', views.list, name='list' ),
+    #path('<int:pk>/detail/', views.MedicationDetailView.as_view(), name='detail'),
 
 ]
--- medicationapp/mysite/drugs/views.py
+++ medicationapp/mysite/drugs/views.py
@@ -4,40 +4,38 @@ from django.shortcuts import render
 from django.views import generic
 from django.views.generic import ListView, DetailView
 
-
-class MedicationListView(ListView):
-    model = Medication
-    template_name = 'drugs/list.html'
-
-    def get_queryset(self):
-        medication_list = Medication.objects.all()
-        return medication_list
-
-
-class MedicationDetailView(DetailView):
-    model = Medication
-    template_name = 'drugs/medication_detail.html'
+#
+# class MedicationListView(ListView):
+#     model = Medication
+#     template_name = 'drugs/list.html'
+#
+#     def get_queryset(self):
+#         medication_list = Medication.objects.all()
+#         return medication_list
 
 
-class SideEffectListView(ListView):
-    model = SideEffect
-    template_name = 'drugs/list.html'
-
-    def get_queryset(self):
-        side_effect_list = SideEffect.objects.all()
-        return side_effect_list
+# class MedicationDetailView(DetailView):
+#     model = Medication
+#     template_name = 'drugs/medication_detail.html'
 
 
-
+# class SideEffectListView(ListView):
+#     model = SideEffect
+#     template_name = 'drugs/list.html'
+#
+#     def get_queryset(self):
+#         side_effect_list = SideEffect.objects.all()
+#         return side_effect_list
+#
 
 
 
-#def list(request):
- #   medication_list = Medication.objects.all()
-  #  side_effect_list = SideEffect.objects.all()
-   # return render(request, 'drugs/list.html', {'medication_list':medication_list,
-                                               #'side_effect_list':side_effect_list})
 
 
+def list(request):
+   print('inside list function')
+   medication_list = Medication.objects.all()
+   side_effect_list = SideEffect.objects.all()
 
-#def detail(request, medication_id):
+   return render(request, 'drugs/list.html', {'medication_list':medication_list,
+                                               'side_effect_list':side_effect_list})
