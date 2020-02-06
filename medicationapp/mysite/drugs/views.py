472c434ef2961891db42259bb548ad4129ba7874
Tilek Aisuluu <aisuluuisabekova@gmail.com>
2020-02-06 14:02:00 +0000

    list of medications with side effects on web page

--- medicationapp/mysite/drugs/views.py
+++ medicationapp/mysite/drugs/views.py
@@ -4,38 +4,21 @@ from django.shortcuts import render
 from django.views import generic
 from django.views.generic import ListView, DetailView
 
-#
-# class MedicationListView(ListView):
-#     model = Medication
-#     template_name = 'drugs/list.html'
-#
-#     def get_queryset(self):
-#         medication_list = Medication.objects.all()
-#         return medication_list
-
-
-# class MedicationDetailView(DetailView):
-#     model = Medication
-#     template_name = 'drugs/medication_detail.html'
-
-
-# class SideEffectListView(ListView):
-#     model = SideEffect
-#     template_name = 'drugs/list.html'
-#
-#     def get_queryset(self):
-#         side_effect_list = SideEffect.objects.all()
-#         return side_effect_list
-#
-
-
-
-
 
 def list(request):
    print('inside list function')
    medication_list = Medication.objects.all()
    side_effect_list = SideEffect.objects.all()
+   #for med in medication_list:
+    #   print(med.medication_name)
+     #  for med.medication_name in medication_list:
+      #     print(med.sideeffect_set.all())
+
 
    return render(request, 'drugs/list.html', {'medication_list':medication_list,
                                                'side_effect_list':side_effect_list})
+
+
+# class MedicationDetailView(DetailView):
+#     model = Medication
+#     template_name = 'drugs/medication_detail.html
